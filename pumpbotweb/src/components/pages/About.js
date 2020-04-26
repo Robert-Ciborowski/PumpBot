0555daec19bdc8861b619b3c09f16f72b85bae40
jamestang12 <homing.tang@ryerson.ca>
2020-04-26 06:19:06 -0400

    Web About Page Created

--- pumpbotweb/src/components/pages/About.js
+++ pumpbotweb/src/components/pages/About.js
@@ -1,6 +1,4 @@
-import React, { useEffect } from "react";
-import Contacts from "../../context/contacts/Contacts";
-import ContactFilter from "../../context/contacts/ContactFilter";
+import React, { useEffect, useContext } from "react";
 import texhIng from "../../image/tech.jpg";
 import reactLogo from "../../image/react.png";
 import nodeLogo from "../../image/node.png";
@@ -10,58 +8,67 @@ import tensorflow from "../../image/tensorflow.png";
 import mongodbLogo from "../../image/mongodb.png";
 import Inspiration from "../../image/Inspiration.png";
 import M from "materialize-css/dist/js/materialize.min.js";
-import { Link } from "react-router-dom";
+import FadeIn from "react-fade-in";
+import ContactContaxt from "../../context/contact/contactContext";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 export const About = () => {
+  const contactContext = useContext(ContactContaxt);
   useEffect(() => {
     //Init Materialize JS
     M.AutoInit();
-  });
+    Aos.init({ duration: 2500 });
+    contactContext.setPage("about");
+  }, []);
+
   return (
     <div>
-      <section className="section section-icons center">
-        <div className="container">
-          <div className="row">
-            <br />
+      <FadeIn>
+        <section className="section section-icons center">
+          <div className="container">
+            <div className="row">
+              <br />
 
-            <div className="col s12 m4">
-              <div className="card-panel">
-                <i className="fab fa-discord fa-3x deep-purple-text text-darken-2"></i>
-                <h5 className="grey-text text-darken-4">Free Access</h5>
-                <p>
-                  Pump Bot is completely free and sends out an alert to Discord
-                  server regrading the listing
-                </p>
-                <br />
-                <br />
+              <div className="col s12 m4">
+                <div className="card-panel">
+                  <i className="fab fa-discord fa-3x deep-purple-text text-darken-2"></i>
+                  <h5 className="grey-text text-darken-4">Free Access</h5>
+                  <p>
+                    Pump Bot is completely free and sends out an alert to
+                    Discord server regrading the listing
+                  </p>
+                  <br />
+                  <br />
+                </div>
               </div>
-            </div>
-            <div className="col s12 m4">
-              <div className="card-panel">
-                <i className="fa fa-database fa-3x deep-purple-text text-darken-2"></i>
-                <h5 className="grey-text text-darken-4">NoSQL Databases</h5>
-                <p>
-                  Pump Bot is also available on Web Application and connected to
-                  NoSQL databse which allow user to view pump & dump alert
-                </p>
-                <br />
+              <div className="col s12 m4">
+                <div className="card-panel">
+                  <i className="fa fa-database fa-3x deep-purple-text text-darken-2"></i>
+                  <h5 className="grey-text text-darken-4">NoSQL Databases</h5>
+                  <p>
+                    Pump Bot is also available on Web Application and connected
+                    to NoSQL databse which allow user to view pump & dump alert
+                  </p>
+                  <br />
+                </div>
               </div>
-            </div>
-            <div className="col s12 m4">
-              <div className="card-panel">
-                <i className="fa fa-bolt fa-3x deep-purple-text text-darken-2"></i>
-                <h5 className="grey-text text-darken-4">Always up to date</h5>
-                <p>
-                  Pump Bot uses Pusher technology which allow us to communicat
-                  to the server in real-time and collect the most up to date
-                  data
-                </p>
-                <br />
+              <div className="col s12 m4">
+                <div className="card-panel">
+                  <i className="fa fa-bolt fa-3x deep-purple-text text-darken-2"></i>
+                  <h5 className="grey-text text-darken-4">Always up to date</h5>
+                  <p>
+                    Pump Bot uses Pusher technology which allow us to communicat
+                    to the server in real-time and collect the most up to date
+                    data
+                  </p>
+                  <br />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </FadeIn>
 
       <section class="section section-testimonial grey lighten-4">
         <div class="container">
@@ -148,17 +155,22 @@ export const About = () => {
         <div className="primary-overlay valign-wrapper">
           <div className="row">
             <div className="col s12 center">
-              <h2>Collect Data and See The Future</h2>
+              <h2 className="dataText">Collect Data and See The Future</h2>
             </div>
           </div>
         </div>
       </section>
-
       <section className="section section-language  lighten-4">
         <br />
         <div className="container">
+          <br />
+          <br />
+
+          <br />
+          <br />
+
           <div className="row">
-            <h3 className="center">
+            <h3 data-aos="fade-in" className="center">
               <span className="deep-purple-text text-darken-1">
                 Technology{" "}
               </span>
@@ -166,28 +178,59 @@ export const About = () => {
             </h3>
             <br />
             <br />
+            <br />
+            <br />
             <div className="row">
               <div className="col s2">
-                <img src={pusherLogo} className="responsive-img" />
+                <img
+                  data-aos="fade-up"
+                  src={pusherLogo}
+                  className="responsive-img"
+                />
               </div>
               <div className="col s2">
-                <img src={reactLogo} className="responsive-img" />
+                <img
+                  data-aos="fade-down"
+                  src={reactLogo}
+                  className="responsive-img"
+                />
               </div>
               <div className="col s2">
-                <img src={tensorflow} className="responsive-img" />
+                <img
+                  data-aos="fade-up"
+                  src={tensorflow}
+                  className="responsive-img"
+                />
               </div>
               <div className="col s2">
-                <img src={nodeLogo} className="responsive-img" />
+                <img
+                  data-aos="fade-down"
+                  src={nodeLogo}
+                  className="responsive-img"
+                />
               </div>
               <div className="col s2">
-                <img src={mongodbLogo} className="responsive-img" />
+                <img
+                  data-aos="fade-up"
+                  src={mongodbLogo}
+                  className="responsive-img"
+                />
               </div>
               <div className="col s2">
-                <img src={pythonLogo} className="responsive-img" />
+                <img
+                  data-aos="fade-down"
+                  src={pythonLogo}
+                  className="responsive-img"
+                />
               </div>
             </div>
+            <br />
+            <br />
+            <br />
+            <br />
           </div>
         </div>
+
         <br />
       </section>
 
@@ -204,16 +247,16 @@ export const About = () => {
       <section className="section section-features  lighten-3">
         <br />
         <div className="container">
-          <h3 className="purple-text text-darken-1 center">
+          <h3 data-aos="fade-in" className="purple-text text-darken-1 center">
             Powerful & Innovative Functionality{" "}
           </h3>
-          <h6 className="grey-text text-darken-2 center">
+          <h6 data-aos="fade-in" className="grey-text text-darken-2 center">
             Combining Machine Learning With Financial Stock Market
           </h6>
           <br />
           <br />
           <div className="row">
-            <div className="col s12 m6">
+            <div data-aos="fade-right" className="col s12 m6">
               <h4>
                 <i className="fab fa-btc"></i> Cryptocurrency Data
               </h4>
@@ -227,7 +270,7 @@ export const About = () => {
                 and keeps them updated in a database.
               </p>
             </div>
-            <div className="col s12 m6">
+            <div data-aos="fade-left" className="col s12 m6">
               <h4>
                 <i className="fab fa-discord"></i> Stock Data
               </h4>
@@ -240,7 +283,7 @@ export const About = () => {
             </div>
           </div>
           <div className="row">
-            <div className="col s12 m6">
+            <div data-aos="fade-right" className="col s12 m6">
               <h4>
                 <i className="fa fa-hdd"></i> TensorFlow
               </h4>
@@ -257,7 +300,7 @@ export const About = () => {
                 April 20th, 2020).
               </p>
             </div>
-            <div className="col s12 m6">
+            <div data-aos="fade-left" className="col s12 m6">
               <h4>
                 <i className="fa fa-chart-line"></i> Stock Data
               </h4>
@@ -277,7 +320,7 @@ export const About = () => {
           </div>
         </div>
         <br />
-        <div className="center">
+        <div data-aos="fade-up" className="center">
           <a
             href="https://github.com/Robert-Ciborowski/PumpBot"
             className="btn btn-large grey"
